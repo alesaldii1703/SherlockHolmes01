e55d704ebfc8c7df28c9905023e305270efdf41c
alesaldii1703 <145026000+alesaldii1703@users.noreply.github.com>
2023-11-15 21:59:43 -0600

    Create culprit_reveal.py

---
+++ culprit_reveal.py
@@ -0,0 +1,30 @@
+# Sherlock Holmes is solving a logic puzzle, but there are errors in the code.
+# Your task is to identify and correct the errors to reveal the solution.
+
+def solve_logic_puzzle():
+    suspects = ['Moriarty', 'Watson', 'Adler', 'Lestrade']
+    clues == 3
+
+    # Holmes eliminates suspects based on clues
+    while clues > 0:
+        if 'Moriarty in suspects:
+            suspects.remove('Moriarty')
+        else:
+            clues -= 1
+
+        elif 'Watson' in suspects
+            suspects.remove('Watson')
+        else:
+            clues += 1
+
+        if 'Adler' in suspects:
+            suspects.remove('Adler')
+        else:
+            clues -= 1
+
+    # The remaining suspect is the culprit
+    culpri = suspects[0]
+    print(f"The culprit is {culprit}")
+
+# Call the function to solve the logic puzzle
+solve_logic_puzzle()
