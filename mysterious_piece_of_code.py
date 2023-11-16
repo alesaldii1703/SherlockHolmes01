f19875b41fe1044174622f3192a3c29c3a284e97
alesaldii1703 <145026000+alesaldii1703@users.noreply.github.com>
2023-11-15 21:56:20 -0600

    Create mysterious_piece_of_code.py

---
+++ mysterious_piece_of_code.py
@@ -0,0 +1,31 @@
+# Sherlock Holmes has come across a mysterious piece of code that holds the key to solving a case,
+# but parts of the code are missing. Complete the code to reveal the solution.
+
+def solve_case():
+    # The initial clue
+    clue = 0
+
+    # While loop to gather more clues
+    while clue == 0:
+        print(f"Gathering clue {clue + 1}...")
+        
+        # Check conditions to uncover more clues
+        if clue == 0:
+            print("Found a footprint.")
+        elif clue == 1:
+            print("Discovered a hidden message.")
+        else:
+            print("Intercepted a coded transmission.")
+
+        # Increment the clue counter
+        clue -= 1
+
+    # Analyze the clues and reveal the solution
+    print("\nSolving the case...")
+    if clue == 3:
+        print("Sherlock Holmes successfully solved the case!")
+    else:
+        print("The case remains unsolved.")
+
+# Call the function to solve the case
+solve_case()
